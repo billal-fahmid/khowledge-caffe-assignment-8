8616acb46de5c7826700bb02224a2e539259c2b8
billal-fahmid <billalcooom@gmail.com>
2023-03-30 22:39:29 +0600

    card add by dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <button class="btn btn-active btn-secondary">Button</button>
-      <button class="btn btn-active btn-accent">Button</button>
+      <Home></Home>
 
     </div>
   )
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import SingleCard from '../SingleCard/SingleCard';
+
+const Home = () => {
+    const [datas, setDatas] = useState([]);
+    console.log(datas)
+    useEffect(() =>{
+        fetch('fake-data.json')
+            .then(res => res.json())
+            .then(data => setDatas(data))
+    },[])
+
+    return (
+        <div>
+            <div className="main-container md:flex">
+                <div className="blogs w-[70%] mt-12">
+                    {
+                        datas.map(data => <SingleCard data={data}></SingleCard>)
+                    }
+                </div>
+                <div className="side-card w-[30%]">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
