4ffb66c7682fdb92b2544152cfc65dc3bacb37d9
billal-fahmid <billalcooom@gmail.com>
2023-03-30 22:21:53 +0600

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1>This is header</h1>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">Knowledge World</a>
+                </div>
+                 <ul className='mr-8 text-xl font-semibold gap-4'>
+                        <li ><a href="/home">Home</a></li>
+                        <li><a href="/latest">Latests</a></li>
+                        <li><a href="/blog">Blog</a></li>
+                    </ul>
+                <div className="flex gap-2">
+                   
+                    <div className="dropdown dropdown-end">
+                        
+                            <div className="w-10 rounded-full">
+                                <img src="icon.png" />
+                            </div>
+                       
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
